7d4142b558ed54d96c8e0f861446e37fdf70ec7c
kishore17kumar <154546334+kishore17kumar@users.noreply.github.com>
2024-09-22 17:22:15 +0530

    4

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     setLoading(true); // Start loading
     try {
       const parsedJson = JSON.parse(jsonInput);
-      const res = await axios.post('https://backend-cxvy.onrender.com', parsedJson);
+      const res = await axios.post('https://backend-cxvy.onrender.com/bfhl', parsedJson);
       setResponse(res.data);
     } catch (error) {
       setError('Invalid JSON or API error'); // Set error message
